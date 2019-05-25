9cd9df15e3eed68605c1147d3b8e1e7368ddc9c0
Clyde D'Souza <clyd94@gmail.com>
2019-05-26 11:54:28 +1200

    Move method definitions above

--- src/uiavatars.ts
+++ src/uiavatars.ts
@@ -55,6 +55,31 @@ interface UIAvatarSettings {
 }
 
 /**
+ * Checks if supplied setting exists and valid and constructs the query string part
+ * of the API URL accordingly.
+ * @param key The key part of the query string.
+ * @param setting The setting that needs to be supplied as the value for that key.
+ */
+function resolveSettings(key:string, setting: any) : string{
+    if(!setting){
+        return "";
+    }
+    return  `${key}=${setting}&`;
+}
+
+/**
+ * Remove the & character from the end of the URL if it exists.
+ * @param apiURL The API URL being constructed
+ */
+function trimAmpersandIfExists(apiURL: string){
+    const doesURLEndWithAmpersand = apiURL.substr(apiURL.length - 1) === "&";
+    if(!doesURLEndWithAmpersand){
+        return apiURL;
+    }
+    return apiURL.substring(stringStartIndex, apiURL.length - stringTrimIndexFromEnd);
+}
+
+/**
  * Generates a UI Avatar image URL that you can supply to your `<img/>` tag.
  * @param settings {UIAvatarSettings} A settings object with any custom values.
  * @returns Returns a image stream to be used directly in an `<img/>` tag.
@@ -79,29 +104,4 @@ export function generateAvatar(settings: UIAvatarSettings): string {
     apiURL = trimAmpersandIfExists(apiURL);
 
     return apiURL;
-}
-
-/**
- * Checks if supplied setting exists and valid and constructs the query string part
- * of the API URL accordingly.
- * @param key The key part of the query string.
- * @param setting The setting that needs to be supplied as the value for that key.
- */
-function resolveSettings(key:string, setting: any) : string{
-    if(!setting){
-        return "";
-    }
-    return  `${key}=${setting}&`;
-}
-
-/**
- * Remove the & character from the end of the URL if it exists.
- * @param apiURL The API URL being constructed
- */
-function trimAmpersandIfExists(apiURL: string){
-    const doesURLEndWithAmpersand = apiURL.substr(apiURL.length - 1) === "&";
-    if(!doesURLEndWithAmpersand){
-        return apiURL;
-    }
-    return apiURL.substring(stringStartIndex, apiURL.length - stringTrimIndexFromEnd);
 }
